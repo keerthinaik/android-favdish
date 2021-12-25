18d8f74355bb97e902434f37aa81101d91d80278
KEERTHINAIK R <keerthinaikr1996@gmail.com>
2021-12-25 22:35:11 +0530

    Refactored to MVVM

--- app/src/main/java/com/example/favdish/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.favdish.ui.dashboard
+package com.example.favdish.view.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.favdish.R
-import com.example.favdish.databinding.FragmentDashboardBinding
+import com.example.favdish.databinding.FragmentHomeBinding
+import com.example.favdish.viewmodel.HomeViewModel
 
-class DashboardFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +25,14 @@ class DashboardFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
